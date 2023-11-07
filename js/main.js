5caf80795fe59df75c954a53a5c60be0fc1e8654
Khaing Zin Htay <ms.khaingzin2003@gmail.com>
2023-11-07 14:35:49 +0630

    index prepare

---
+++ js/main.js
@@ -0,0 +1,90 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+menu.onclick =() => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('header').classList.add('active');
+    }else{
+        document.querySelector('header').classList.remove('active');
+    }
+}
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+   autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  });
+  var swiper = new Swiper(".feature-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+   autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  breakpoints:{
+    0:{
+        slidesPerView:1,
+    },
+    768:{
+        slidesPerView:2,
+    },
+    991:{
+        slidesPerView:3,
+    },
+  }
+  });
+  var swiper = new Swiper(".trainer-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+   autoplay: {
+    delay: 600,
+    disableOnInteraction: false,
+  },
+  breakpoints:{
+    0:{
+        slidesPerView:1,
+    },
+    768:{
+        slidesPerView:2,
+    },
+    991:{
+        slidesPerView:3,
+    },
+  }
+  });
+  var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+   autoplay: {
+    delay: 600,
+    disableOnInteraction: false,
+  },
+  breakpoints:{
+    0:{
+        slidesPerView:1,
+    },
+    768:{
+        slidesPerView:2,
+    },
+    991:{
+        slidesPerView:3,
+    },
+  }
+  });
